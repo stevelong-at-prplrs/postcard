ef762de255967e8b8647af419d21081140186834
stevelong-at-prplrs <76882223+stevelong-at-prplrs@users.noreply.github.com>
2024-06-22 19:54:35 -0500

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { loadResource } from "./helpers";
 import { loadResource } from "mod-pkg";
 loadResource('/bundle.js');
 
