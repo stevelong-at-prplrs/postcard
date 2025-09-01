aa10ff90929608d117c012c195898844417cf5f4
stevelong-at-prplrs <76882223+stevelong-at-prplrs@users.noreply.github.com>
2025-09-01 16:18:35 -0500

    don't hit server API, use only wayne

--- src/sw.js
+++ src/sw.js
@@ -4,16 +4,23 @@ import { genResponse, serverListen, methodArr } from './utils';
 
 const app = new Wayne();
 
+// original serverListen method
+// serverListen(methodArr, app, method => async (req, res) => {
+//     try {
+//         const networkResponse = await fetch(req.url, {
+//             method,
+//             headers: req.headers
+//         }).catch(() => ({ status: 500, statusText: 'Internal Server Error', ok: false }));
+//         if (networkResponse && networkResponse.status === 200 && networkResponse.statusText === 'OK' && networkResponse.ok) {
+//             return res.text(await networkResponse.text());
+//         } else { // replace response if network request fails
+//             return res.text(genResponse(200, method.toUpperCase(), req));
+//         }
+//     } catch (_e) {}
+// });
+
 serverListen(methodArr, app, method => async (req, res) => {
     try {
-        const networkResponse = await fetch(req.url, {
-            method,
-            headers: req.headers
-        }).catch(() => ({ status: 500, statusText: 'Internal Server Error', ok: false }));
-        if (networkResponse && networkResponse.status === 200 && networkResponse.statusText === 'OK' && networkResponse.ok) {
-            return res.text(await networkResponse.text());
-        } else { // replace response if network request fails
-            return res.text(genResponse(200, method.toUpperCase(), req));
-        }
+        return res.text(genResponse(200, method.toUpperCase(), req));
     } catch (_e) {}
 });
